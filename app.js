12958b575198e647d8953c94928ca35270fc6e89
Olga <171145876+olgarunkova@users.noreply.github.com>
2024-10-04 12:07:24 -0700

    Add backspace button

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const equality = document.querySelector(".equality");
+const dot = document.querySelector(".dot");
+const backspace = document.querySelector(".backspace");
 const clean = document.querySelector(".clean");
 const expressionDisplay = document.querySelector(".expression");
 const resultDisplay = document.querySelector(".result");
@@ -16,8 +18,10 @@ let currentState = "waitingForTheFirstNumber";
 numbers.forEach((button) => {
     button.addEventListener("click", () => {
        let numValue = button.dataset.value;
-       let result;
-       if (currentState === "waitingForTheFirstNumber") {
+       if (currentState === "waitingForTheFirstNumber"
+       || currentState === "waitingForTheOperator"
+       ) {
+        //num1 = "";
         num1 += numValue;
         expressionDisplay.textContent = `${num1}`;
         resultDisplay.textContent = `${num1}`;
@@ -33,7 +37,7 @@ numbers.forEach((button) => {
 
 operators.forEach((button) => {
     button.addEventListener("click", () => {
-        currentState = "waitingForTheOperator"
+        currentState = "waitingForTheOperator";
         if (currentState === "waitingForTheOperator") {
             operator = button.dataset.value;
             expressionDisplay.textContent += ` ${operator} `;
@@ -44,6 +48,7 @@ operators.forEach((button) => {
 
 equality.addEventListener("click", () => {
     currentState = "waitingForResult";
+    let result;
     if (currentState === "waitingForResult") {
         result = operate();
         resultDisplay.textContent = `${result}`;
@@ -53,6 +58,40 @@ equality.addEventListener("click", () => {
         currentState = "waitingForTheOperator";
         expressionDisplay.textContent = `${result}`;
        };
+});
+
+dot.addEventListener("click", () => {
+    let dotValue = dot.dataset.value;
+    if (currentState === "waitingForTheFirstNumber") {
+        if (!num1.includes(".")) {
+            num1 += dotValue;
+            expressionDisplay.textContent = `${num1}`;
+            resultDisplay.textContent = `${num1}`;
+        }
+     
+    } else if (currentState === "waitingForTheSecondNumber") {
+        if (!num2.includes(".")) {
+            num2 += dotValue;
+            expressionDisplay.textContent = `${num1} ${operator} ${num2}`;
+            resultDisplay.textContent = `${num2}`;
+        }
+    };
+});
+
+backspace.addEventListener("click", () => {
+    if (currentState === "waitingForTheFirstNumber") {
+        num1 = num1.slice(0, -1);
+        expressionDisplay.textContent = `${num1}`;
+        resultDisplay.textContent = `${num1}`;
+    }  else if (currentState === "waitingForTheSecondNumber" && num2) {
+        num2 = num2.slice(0, -1);
+        expressionDisplay.textContent = `${num1} ${operator} ${num2}`;
+        resultDisplay.textContent = `${num2}`;
+    } else if (currentState === "waitingForTheSecondNumber") {
+        operator = operator.slice(0, -1);
+        expressionDisplay.textContent = `${num1}`;
+        resultDisplay.textContent = `${num1}`;
+    }
 })
 
 clean.addEventListener("click", () => {
@@ -61,7 +100,7 @@ clean.addEventListener("click", () => {
     expressionDisplay.textContent = "";
     resultDisplay.textContent = "";
     currentState = "waitingForTheFirstNumber";
-})
+});
 
 
 function operate() {
