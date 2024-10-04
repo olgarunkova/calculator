fb91503bf30ed859eff1405d9d642c583809d6ea
Olga <171145876+olgarunkova@users.noreply.github.com>
2024-10-04 05:47:48 -0700

    Add functionality so now it handles multiple digit numbers

--- app.js
+++ app.js
@@ -1,44 +1,39 @@
-const items = document.querySelectorAll(".item");
+const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
+const equality = document.querySelector(".equality");
+const clean = document.querySelector(".clean");
 const expressionDisplay = document.querySelector(".expression");
 const resultDisplay = document.querySelector(".result");
 
-let num1 = 0;
-let num2 = 0;
+let num1 = "";
+let num2 = "";
 let operator = "";
 let currentState = "waitingForTheFirstNumber";
 
 
-items.forEach((button) => {
+
+
+numbers.forEach((button) => {
     button.addEventListener("click", () => {
-       let numValue = parseInt(button.dataset.value);
+       let numValue = button.dataset.value;
        let result;
        if (currentState === "waitingForTheFirstNumber") {
-        num1 = numValue;
+        num1 += numValue;
         expressionDisplay.textContent = `${num1}`;
         resultDisplay.textContent = `${num1}`;
-        currentState = "waitingForTheOperator";
-
+        
        } else if (currentState === "waitingForTheSecondNumber") {
-        num2 = numValue;
-        expressionDisplay.textContent += `${num2}`;
+        num2 += numValue;
+        expressionDisplay.textContent = `${num1} ${operator} ${num2}`;
         resultDisplay.textContent = `${num2}`;
-        currentState = "waitingForResult";
 
-       } else if (currentState === "waitingForResult") {
-        result = operate();
-        resultDisplay.textContent = `${result}`;
-        expressionDisplay.textContent += ` = ${result}`;
-        num1 = result;
-        num2 = 0;
-        currentState = "waitingForTheOperator";
-        expressionDisplay.textContent = `${result}`;
        };
     });
 });
 
 operators.forEach((button) => {
     button.addEventListener("click", () => {
+        currentState = "waitingForTheOperator"
         if (currentState === "waitingForTheOperator") {
             operator = button.dataset.value;
             expressionDisplay.textContent += ` ${operator} `;
@@ -47,8 +42,31 @@ operators.forEach((button) => {
     });
 });
 
+equality.addEventListener("click", () => {
+    currentState = "waitingForResult";
+    if (currentState === "waitingForResult") {
+        result = operate();
+        resultDisplay.textContent = `${result}`;
+        expressionDisplay.textContent += ` = ${result}`;
+        num1 = result.toString();
+        num2 = "";
+        currentState = "waitingForTheOperator";
+        expressionDisplay.textContent = `${result}`;
+       };
+})
+
+clean.addEventListener("click", () => {
+    num1 = "";
+    num2 = "";
+    expressionDisplay.textContent = "";
+    resultDisplay.textContent = "";
+    currentState = "waitingForTheFirstNumber";
+})
+
 
 function operate() {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
     let result;
     switch (operator) {
         case "+":
