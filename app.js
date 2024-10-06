219b4dd7d4f5687c54ff03c0bb144e556dd38564
Olga <171145876+olgarunkova@users.noreply.github.com>
2024-10-06 09:57:38 -0700

    Add character limit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const equality = document.querySelector(".equality");
+const percentage = document.querySelector(".percentage");
+const plusMinus = document.querySelector(".plusMinus");
 const dot = document.querySelector(".dot");
 const backspace = document.querySelector(".backspace");
 const clean = document.querySelector(".clean");
@@ -11,7 +13,7 @@ let num1 = "";
 let num2 = "";
 let operator = "";
 let currentState = "waitingForTheFirstNumber";
-
+const MAX_LENGTH = 11;
 
 
 
@@ -21,16 +23,18 @@ numbers.forEach((button) => {
        if (currentState === "waitingForTheFirstNumber"
        || currentState === "waitingForTheOperator"
        ) {
-        //num1 = "";
-        num1 += numValue;
-        expressionDisplay.textContent = `${num1}`;
-        resultDisplay.textContent = `${num1}`;
+        if (num1.length < MAX_LENGTH) {
+            num1 += numValue;
+            expressionDisplay.textContent = `${num1}`;
+            resultDisplay.textContent = `${num1}`;
+        };
         
        } else if (currentState === "waitingForTheSecondNumber") {
-        num2 += numValue;
-        expressionDisplay.textContent = `${num1} ${operator} ${num2}`;
-        resultDisplay.textContent = `${num2}`;
-
+        if (num2.length < MAX_LENGTH) {
+            num2 += numValue;
+            expressionDisplay.textContent = `${num1} ${operator} ${num2}`;
+            resultDisplay.textContent = `${num2}`;
+        };
        };
     });
 });
@@ -45,7 +49,7 @@ operators.forEach((button) => {
            };
     });
 });
-
+  
 equality.addEventListener("click", () => {
     currentState = "waitingForResult";
     let result;
@@ -60,6 +64,41 @@ equality.addEventListener("click", () => {
        };
 });
 
+percentage.addEventListener("click", () => {
+    if (currentState === "waitingForTheFirstNumber") {
+        num1 = (parseFloat(num1) / 100).toString();  
+        expressionDisplay.textContent = `${num1}`;
+        resultDisplay.textContent = `${num1}`;
+    } else if (currentState === "waitingForTheSecondNumber") {
+        num2 = (parseFloat(num2) / 100).toString();  
+        expressionDisplay.textContent = `${num1} ${operator} ${num2}`;
+        resultDisplay.textContent = `${num2}`;
+    } else if (currentState === "waitingForTheOperator") {
+        let pResult = (parseFloat(resultDisplay.textContent) / 100).toString(); 
+        resultDisplay.textContent = `${pResult}`;
+        expressionDisplay.textContent += ` = ${pResult}`;
+    };
+});
+
+plusMinus.addEventListener("click", () => {
+    if (currentState === "waitingForTheFirstNumber") {
+        num1 =  (num1 > 0) ? (-parseFloat(num1)).toString() : (num1 * (-1)).toString();  
+        expressionDisplay.textContent = `${num1}`;
+        resultDisplay.textContent = `${num1}`;
+    } else if (currentState === "waitingForTheSecondNumber") {
+        num2 = (num2 > 0) ? (-parseFloat(num2)).toString() : (num2 * (-1)).toString();  
+        expressionDisplay.textContent = `${num1} ${operator} (${num2})`;
+        resultDisplay.textContent = `${num2}`;
+    } else if (currentState === "waitingForTheOperator") {
+        let currentResult = parseFloat(resultDisplay.textContent);
+        let plusMinusResult = (currentResult > 0) ? (-currentResult).toString() : (currentResult * (-1)).toString();
+        expressionDisplay.textContent = `${plusMinusResult}`;
+        resultDisplay.textContent = `${plusMinusResult}`;
+        num1 = plusMinusResult;
+        num2 = "";
+    };
+});
+
 dot.addEventListener("click", () => {
     let dotValue = dot.dataset.value;
     if (currentState === "waitingForTheFirstNumber") {
@@ -67,9 +106,9 @@ dot.addEventListener("click", () => {
             num1 += dotValue;
             expressionDisplay.textContent = `${num1}`;
             resultDisplay.textContent = `${num1}`;
-        }
-     
-    } else if (currentState === "waitingForTheSecondNumber") {
+        } 
+
+        } else if (currentState === "waitingForTheSecondNumber") {
         if (!num2.includes(".")) {
             num2 += dotValue;
             expressionDisplay.textContent = `${num1} ${operator} ${num2}`;
@@ -91,8 +130,15 @@ backspace.addEventListener("click", () => {
         operator = operator.slice(0, -1);
         expressionDisplay.textContent = `${num1}`;
         resultDisplay.textContent = `${num1}`;
-    }
-})
+    } else if (currentState === "waitingForTheOperator") {
+        let  backResult = resultDisplay.textContent.toString().slice(0, -1); 
+        resultDisplay.textContent = `${backResult}`;
+        expressionDisplay.textContent = "";
+        expressionDisplay.textContent += ` = ${backResult}`;
+        num1 = "";
+        num2 = "";
+    };
+});
 
 clean.addEventListener("click", () => {
     num1 = "";
@@ -106,6 +152,7 @@ clean.addEventListener("click", () => {
 function operate() {
     num1 = parseFloat(num1);
     num2 = parseFloat(num2);
+    if (num2 === "") num2 = num1;
     let result;
     switch (operator) {
         case "+":
@@ -120,9 +167,9 @@ function operate() {
         case "/":
             result = num1 / num2;
             break;
-    }
+    };
     return result;
-}
+};
 
 
 
